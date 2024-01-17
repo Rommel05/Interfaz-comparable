b8dd824d8253044c5044cfbfbb4ba1ca4a0e1fb9
Ryze <148451192+Rommel05@users.noreply.github.com>
2024-01-17 19:49:29 +0100

    Add files via upload

---
+++ src/Localidad1.java
@@ -0,0 +1,15 @@
+public class Localidad1 {
+    private int dineroInvertido;
+    private int premio;
+    public Localidad1(int dineroInvertido, int premio) {
+        this.dineroInvertido = dineroInvertido;
+        this.premio = premio;
+    }
+    public int getdineroInvertido() {
+        return this.dineroInvertido;
+    }
+    public int getPremio() {
+        return this.premio;
+    }
+}
+
