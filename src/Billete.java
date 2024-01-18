62e3fd7fd528733a733ef27da6abdf9df2007ef6
Ryze <148451192+Rommel05@users.noreply.github.com>
2024-01-18 10:06:19 +0100

    Add files via upload

---
+++ src/Billete.java
@@ -0,0 +1,15 @@
+public class Billete  {
+    private int dineroInvertido;
+    private int premio;
+    public Billete (int dineroInvertido, int premio) {
+        this.dineroInvertido = dineroInvertido;
+        this.premio = premio;
+    }
+
+    public int getDineroInvertido() {
+        return this.dineroInvertido;
+    }
+    public int getPremio() {
+        return this.premio;
+    }
+}
---
+++ src/Loteria.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Loteria {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int numero = 1;
+        System.out.println("Introduce un las cuidades, si no hay ninguna pon 0:");
+        while (true) {
+            numero = sc.nextInt();
+            if(numero == 0) {
+                break;
+            }
+            ArrayList<Billete> billetes = new ArrayList<>();
+            for (int i = 0; i < numero; i++) {
+                añadirbilletes(billetes);
+            }
+            Comparator<Billete> comparator = Comparator
+                    .comparing(Billete::getDineroInvertido)
+                    .thenComparing(Billete::getPremio);
+            Collections.sort(billetes, comparator);
+            if (justo(billetes)) {
+                System.out.println("Si");
+            } else {
+                System.out.println("No");
+            }
+        }
+    }
+    public static void añadirbilletes(ArrayList<Billete> billetes) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el dinero invertido");
+        int dineroInvertido = sc.nextInt();
+        System.out.println("Introduce el premio");
+        int premio = sc.nextInt();
+        Billete billete1 = new Billete(dineroInvertido, premio);
+        billetes.add(billete1);
+    }
+    public static boolean justo(ArrayList<Billete> billetes) {
+        for (int i = 1; i < billetes.size(); i++) {
+            if (billetes.get(i).getPremio() < billetes.get(i - 1).getPremio()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
